ec31d027e1a9af1c2f468921964077e16bc529ef
billcookie0929 <1103111429@qq.com>
2020-03-28 14:28:34 +0800

    ？

--- Basic Function/Caculator.py
+++ Basic Function/Caculator.py
@@ -3,6 +3,6 @@
 # @Time    : 2020/3/13 20:40
 # @Author  : Yupeng Cui  
 # @File    : Caculator.py
-import math as m
-def input_instruct ():
-    str_1 = eval(input("请输入要计算的表达式："))
+import tkinter as tk
+
+class Calculator:
